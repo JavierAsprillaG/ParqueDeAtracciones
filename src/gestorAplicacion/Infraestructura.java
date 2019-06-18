7d61dfa8f6f7c38a2007af9a7e9f70696aceb5aa
JavierAsprillaG <lasprilla@unal.edu.co>
2019-06-17 23:52:31 -0500

    Comienzo de la Aventura (n_n)b
    Cimientos del proshecto

---
+++ src/gestorAplicacion/Infraestructura.java
@@ -0,0 +1,13 @@
+package gestorAplicacion;
+
+public abstract class Infraestructura {
+	private static int ID;
+    private String nombre;
+    private int ganancias;
+    
+    public Infraestructura(int id, String nom, int gan){
+    	ID = id; 
+    	nombre = nom;
+    	ganancias = gan;
+    }
+}
---
+++ src/uiMain/MenuConsola.java
@@ -0,0 +1,5 @@
+package uiMain;
+
+abstract class MenuConsola {
+
+}
