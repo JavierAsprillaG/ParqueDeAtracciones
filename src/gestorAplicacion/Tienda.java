fa6d47ceb3177bd77a1b0f7d746dfd32bbfc1255
crjahe26 <crjaramillohe@unal.edu.co>
2019-06-24 13:32:23 -0500

    metodos de tienda
    Agregue los metodos para manejar el inventario de la tienda y explique que hace cada metodo.

--- src/gestorAplicacion/Tienda.java
+++ src/gestorAplicacion/Tienda.java
@@ -7,4 +7,20 @@ public class Tienda extends Infraestructura {
 		super(id, nom, gan);
 		// TODO Auto-generated constructor stub
 		}
+	public void agregarProducto(int k, int e) {
+		inventario.put(k, e);
+		//agrega el producto de codigo k y su respectiva cantidad e
 	}
+	public int consultarInventario(int k) {
+		int c = inventario.get(k);
+		return c;
+		//se busca el producto de codigo k y devuelve la cantidad que hay de tal objeto
+	}
+	public void eliminarProducto(int k) {
+		inventario.remove(k);
+		//elimina el producto de codigo k del inventario
+	}
+	}
+// elemento e con clave k = put (k, e)
+// clave es el codigo del producto 
+// elemento es la cantidad de dicho producto
