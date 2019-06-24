bf996a61ae4e8bb9753ecc01276724e59d6d3631
ancgarciamo <ancgarciamo@unal.edu.co>
2019-06-24 12:08:41 -0500

    Herencia

--- src/gestorAplicacion/Atraccion.java
+++ src/gestorAplicacion/Atraccion.java
@@ -4,7 +4,7 @@ public class Atraccion extends Infraestructura{
 	private boolean estado;	
 	public Atraccion(int id,String nom,int gan, int cap,boolean est) {
 		super(id,nom,gan);
-		capacidad=capa;
+		capacidad=cap;
 		estado=est;	
 	}
 }
